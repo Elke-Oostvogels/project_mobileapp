51aeb93f3993eaab352cde265d431cc0ab566287
Elke <r0830699@student.thomasmore.be>
2021-11-29 15:35:48 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'project_Scheldeoord',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
